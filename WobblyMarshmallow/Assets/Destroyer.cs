a3488fd9d70f3f084407760925708cc55bdd5a6b
nandanvinjamury <nvinjamury@gmail.com>
2018-09-09 14:43:41 -0700

    testing falling marshmallows and player movement

---
+++ WobblyMarshmallow/Assets/Destroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+	private void OnCollisionStay(Collision collision) {
+		if (collision.gameObject.tag.Equals("Finish")) {
+			Destroy(collision.gameObject);
+		}
+	}
+}
